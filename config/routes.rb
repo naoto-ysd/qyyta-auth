e929fbe0d7396bd579d4273feea0841afc33bcd2
Yoshida <61533915+naoto-ysd@users.noreply.github.com>
2024-01-29 04:16:23 +0900

    create sign up feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
+  namespace :api do
+    namespace :v1 do
+      # mount_devise_token_auth_for 'User', at: 'auth'
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
+    end
+
+    namespace :auth do
+      resources :sessions, only: :index
+    end
+
+  end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
